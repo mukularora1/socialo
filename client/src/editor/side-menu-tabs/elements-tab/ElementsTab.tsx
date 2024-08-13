05e8fc28d54fee63211c7bdb6c06227303beac25
mukul arora <mukularora7737@gmail.com>
2024-08-13 20:48:20 +0530

    fix: drag and drop functionality and element push into canvas

--- client/src/editor/side-menu-tabs/elements-tab/ElementsTab.tsx
+++ client/src/editor/side-menu-tabs/elements-tab/ElementsTab.tsx
@@ -1,8 +1,43 @@
-import img from "../../../assets/images/login.png";
+import { useDispatch } from "react-redux";
 import InputBox from "../../../components/input-box/InputBox";
 import ResponsiveImg from "../../../components/responsive-img/ResponsiveImg";
+import { addObjects } from "../../store/editorSlice";
 import "./elementsTab.css";
+const elements = [
+  {
+    id: 1,
+    url: "/circle-filled-svgrepo-com.svg",
+  },
+  {
+    id: 2,
+    url: "/square-heart-svgrepo-com.svg",
+  },
+  {
+    id: 3,
+    url: "/square-svgrepo-com-curve.svg",
+  },
+  {
+    id: 4,
+    url: "/square-svgrepo-com.svg",
+  },
+];
 function ElementsTab() {
+  const dispatch = useDispatch();
+  const handleElementClick = (url: string) => {
+    dispatch(
+      addObjects({
+        type: "Image",
+        options: {
+          url: `http://localhost:5173${url}`,
+          left: 100, // Example position
+          top: 100, // Example position
+          width: 200, // Example dimensions
+          height: 200, // Example dimensions
+        },
+        id: Math.random(),
+      })
+    );
+  };
   return (
     <div className="elements-tab">
       <InputBox placeholder="Search svg, shapes etc" />
@@ -17,15 +52,14 @@ function ElementsTab() {
             </div>
           </div>
           <div className="elements-tab__element-groups__group-wrapper__element-wrapper">
-            <div className="elements-tab__element-groups__group-wrapper__element-wrapper--element">
-              <ResponsiveImg src={img} alt="hello world" />
-            </div>
-            <div className="elements-tab__element-groups__group-wrapper__element-wrapper--element">
-              <ResponsiveImg src={img} alt="hello world" />
-            </div>
-            <div className="elements-tab__element-groups__group-wrapper__element-wrapper--element">
-              <ResponsiveImg src={img} alt="hello world" />
-            </div>
+            {elements.map(({ id, url }) => (
+              <div
+                key={id}
+                className="elements-tab__element-groups__group-wrapper__element-wrapper--element"
+                onClick={() => handleElementClick(url)}>
+                <ResponsiveImg src={url} alt={`Element ${id}`} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,5 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import editorReducer from "../editor/store/editorSlice";
 import publisherReducer from "../social-media-management/store/publisherSlice";
 export default configureStore({
-  reducer: { publisherReducer, editorReducer },
+  reducer: { publisherStore: publisherReducer, editorStore: editorReducer },
 });
