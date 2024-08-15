24f0015e4b0d43d7d894b5a366c18c49b47ccd09
mukul arora <mukularora7737@gmail.com>
2024-08-16 01:15:29 +0530

    fix: text tab created and admin page created

---
+++ client/src/editor/side-menu-tabs/text-tab/TextTab.tsx
@@ -0,0 +1,16 @@
+import "./textTab.css";
+
+function TextTab() {
+  return (
+    <div className="text-tab">
+      <div>Texts</div>
+      <div>
+        <div>Heading</div>
+        <div>Sub heading</div>
+        <div>Paragraph</div>
+      </div>
+    </div>
+  );
+}
+
+export default TextTab;
