d23da002f8b5b01406e7aa15dcafd931f7873b37
Mukul Arora <103983993+mukularora1@users.noreply.github.com>
2024-08-18 01:09:40 +0530

    Ma editor (#20)
    * fix: image added to canvas
    
    * fix: drag and drop functionality and element push into canvas
    
    * fix: fabric events added
    
    * fix: color picker and selection event funcitonal
    
    * fix: svg changes
    
    * fix: editable text and svg load into canvas
    
    * fix: text tab created and admin page created
    
    * fix: sidemenu configured
    
    * fix: sidemenu configured
    
    * fix: selected item updated
    
    * fix: sidemenu diff tabs design done

--- client/src/editor/side-menu-tabs/images-tab/ImagesTab.tsx
+++ client/src/editor/side-menu-tabs/images-tab/ImagesTab.tsx
@@ -1,6 +1,17 @@
 import "./imagesTab.css";
 function ImagesTab() {
-  return <div>ImagesTab</div>;
+  return (
+    <div className="images-tab">
+      <div>images</div>
+      <div className="images-tab__images-wrapper">
+        <div className="images-tab__images-wrapper__image"></div>
+        <div className="images-tab__images-wrapper__image"></div>
+        <div className="images-tab__images-wrapper__image"></div>
+        <div className="images-tab__images-wrapper__image"></div>
+        <div className="images-tab__images-wrapper__image"></div>
+      </div>
+    </div>
+  );
 }
 
 export default ImagesTab;
--- client/src/editor/side-menu-tabs/pages-tab/PagesTab.tsx
+++ client/src/editor/side-menu-tabs/pages-tab/PagesTab.tsx
@@ -1,6 +1,19 @@
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import "./pagesTab.css";
 function PagesTab() {
-  return <div>PagesTab</div>;
+  return (
+    <div className="page-tab">
+      <div>pages</div>
+      <div className="page-tab__pages-wrapper">
+        <div className="page-tab__pages-wrapper__page">
+          <AddOutlinedIcon />
+        </div>
+        <div className="page-tab__pages-wrapper__page">
+          <AddOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PagesTab;
